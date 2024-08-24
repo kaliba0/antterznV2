54404a7a2b651de1dc23d74b33936c8bb73a7de2
mt-zn <mterzian@gmail.com>
2024-08-24 16:39:30 +0200

    menu pr product.html

--- dropsownmenu.js
+++ dropsownmenu.js
@@ -3,23 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeBtn = document.getElementById('close');
     const showProducts = document.getElementById('products');
     const showProductsIcon = document.getElementById('showProductsIcon');
-    const subProducts = document.getElementById('sub-products')
+    const subProducts = document.getElementById('sub-products');
     const dropdownMenu = document.getElementById('dropdownmenu');
+    const body = document.body;
+    let scrollPosition = 0;
 
     menuBtn.addEventListener('click', function() {
-        // Toggle la classe "show" sur le dropdown menu
-        dropdownMenu.classList.toggle('show');
+        scrollPosition = window.scrollY;
+        body.style.overflow = 'hidden';
+        body.style.position = 'fixed';
+        body.style.top = `-${scrollPosition}px`;
+        body.style.width = '100%';
+        dropdownMenu.classList.add('show');
     });
 
     closeBtn.addEventListener('click', function() {
-        // Toggle la classe "show" sur le dropdown menu
-        dropdownMenu.classList.toggle('show');
+        body.style.overflow = '';
+        body.style.position = '';
+        body.style.top = '';
+        body.style.width = '';
+        window.scrollTo(0, scrollPosition);
+        dropdownMenu.classList.remove('show');
     });
 
     showProducts.addEventListener('click', function() {
-        // Toggle la classe "show" sur le dropdown menu
+        // Basculer l'affichage du sous-menu avec effet de glissement
         subProducts.classList.toggle('show');
         showProductsIcon.classList.toggle('rotate-90');
-
     });
 });
